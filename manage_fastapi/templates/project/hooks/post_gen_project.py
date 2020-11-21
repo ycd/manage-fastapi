5ee59688c69ba5e0aa12fcb2871064d45cd2eab6
Marcelo Trylesinski <marcelotryle@gmail.com>
2020-11-21 22:58:43 +0100

    :art: Add .gitignore on generated project

--- manage_fastapi/templates/project/hooks/post_gen_project.py
+++ manage_fastapi/templates/project/hooks/post_gen_project.py
@@ -2,20 +2,34 @@ import os
 
 from manage_fastapi.constants import Packaging
 
-packaging = "{{ cookiecutter.packaging }}"
-base_dir = os.getcwd()
 
-remove_paths = []
-if packaging == str(Packaging.PIP):
-    remove_paths = ["poetry.lock", "pyproject.toml"]
-elif packaging == str(Packaging.POETRY):
-    remove_paths = ["requirements.txt"]
+def remove_paths(paths: list):
+    base_dir = os.getcwd()
+
+    for path in paths:
+        path = os.path.join(base_dir, path)
+        if path and os.path.exists(path):
+            if os.path.isdir(path):
+                os.rmdir(path)
+            else:
+                os.unlink(path)
 
 
-for path in remove_paths:
-    path = os.path.join(base_dir, path)
-    if path and os.path.exists(path):
-        if os.path.isdir(path):
-            os.rmdir(path)
-        else:
-            os.unlink(path)
+def set_packaging():
+    packaging = "{{ cookiecutter.packaging }}"
+    paths = []
+
+    if packaging == str(Packaging.PIP):
+        paths = ["poetry.lock", "pyproject.toml"]
+    elif packaging == str(Packaging.POETRY):
+        paths = ["requirements.txt"]
+
+    remove_paths(paths)
+
+
+def main():
+    set_packaging()
+
+
+if __name__ == "__main__":
+    main()
