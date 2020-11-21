5ffda35c3dc1344c3b64ad49304bdd71ef2e72ce
Marcelo Trylesinski <marcelotryle@gmail.com>
2020-11-21 19:27:37 +0100

    :recycle: Change the structure completely

--- manage_fastapi/main.py
+++ manage_fastapi/main.py
@@ -1,22 +1,46 @@
-import pkg_resources
 import typer
+from bullet import Bullet, Input, SlidePrompt, colors
+
+from manage_fastapi.constants import PYTHON_VERSIONS, Packaging
+from manage_fastapi.generator import generate_project
+from manage_fastapi.schemas import Context
+
+app = typer.Typer(help="Managing FastAPI projects made easy!", name="Manage FastAPI")
 
 
-from .project_utils import select_database, start_app, start_project
+@app.command(help="Creates a FastAPI project.")
+def startproject(default: bool = typer.Option(False)):
+    if default:
+        context = Context(name="fastapi-project", packaging=Packaging.PIP, python="3.8")
+    else:
+        cli = SlidePrompt(
+            [
+                Input("Insert the project's name: "),
+                Bullet(
+                    prompt="Choose the package manager:",
+                    choices=list(Packaging),
+                    bullet=" >",
+                    margin=2,
+                    word_color=colors.foreground["white"],
+                    word_on_switch=colors.foreground["white"],
+                ),
+                Bullet(
+                    prompt="Select Python version:",
+                    choices=PYTHON_VERSIONS,
+                    bullet=" >",
+                    margin=2,
+                    word_color=colors.foreground["white"],
+                    word_on_switch=colors.foreground["white"],
+                ),
+            ],
+        )
+        result = cli.launch()
+        context = Context(
+            name=result[0][1], packaging=result[1][1], python=result[2][1]
+        )
+    generate_project(context)
 
-app = typer.Typer(
-    add_completion=False,
-    help="Managing FastAPI projects made easy!",
-    name="Manage FastAPI",
-)
 
-
-@app.command(help="Creates a project with the given name.")
-def startproject(projectname: str = typer.Argument(...)):
-    database_option = select_database()
-    start_project(projectname, database_option=database_option)
-
-
-@app.command(help="Creates a app with the given name.")
-def startapp(appname: str = typer.Argument(...)):
-    start_app(appname)
+@app.command(help="Creates a FastAPI component.")
+def startapp():
+    ...
---
+++ manage_fastapi/schemas.py
@@ -0,0 +1,33 @@
+import subprocess
+
+from pydantic import BaseModel, EmailStr, root_validator, validator
+
+from manage_fastapi.config import FASTAPI_VERSION
+from manage_fastapi.constants import PYTHON_VERSIONS, Packaging
+
+
+class Context(BaseModel):
+    name: str
+    packaging: Packaging
+
+    username: str
+    email: EmailStr
+
+    python: str
+    fastapi: str = FASTAPI_VERSION
+
+    @root_validator(pre=True)
+    def git_info(cls, values: dict):
+        values["username"] = subprocess.check_output(
+            ["git", "config", "--get", "user.name"]
+        )
+        values["email"] = subprocess.check_output(
+            ["git", "config", "--get", "user.email"]
+        )
+        return values
+
+    @validator("python")
+    def validate_python_version(cls, value):
+        if value not in PYTHON_VERSIONS:
+            raise ValueError(f"Invalid Python version: {value}.")
+        return value
