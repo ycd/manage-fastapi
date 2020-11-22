feeccca1c49a3c95e1fb9ea00a72828d3b9dc125
Marcelo Trylesinski <marcelotryle@gmail.com>
2020-11-22 05:08:44 +0100

    :white_check_mark: Add startproject tests

--- manage_fastapi/main.py
+++ manage_fastapi/main.py
@@ -2,10 +2,10 @@ import os
 import subprocess
 
 import typer
-from bullet import Bullet, SlidePrompt, colors
 
 from manage_fastapi.constants import PYTHON_VERSIONS, Packaging
 from manage_fastapi.generator import generate_project
+from manage_fastapi.helpers import bullet, launch_cli
 from manage_fastapi.schemas import Context
 
 app = typer.Typer(help="Managing FastAPI projects made easy!", name="Manage FastAPI")
@@ -16,28 +16,15 @@ def startproject(name: str, default: bool = typer.Option(False)):
     if default:
         context = Context(name=name, packaging=Packaging.PIP, python="3.8")
     else:
-        cli = SlidePrompt(
-            [
-                Bullet(
-                    prompt="Choose the package manager:",
-                    choices=list(Packaging),
-                    bullet=" >",
-                    margin=2,
-                    word_color=colors.foreground["white"],
-                    word_on_switch=colors.foreground["white"],
-                ),
-                Bullet(
-                    prompt="Select Python version:",
-                    choices=PYTHON_VERSIONS,
-                    bullet=" >",
-                    margin=2,
-                    word_color=colors.foreground["white"],
-                    word_on_switch=colors.foreground["white"],
-                ),
-            ],
+        result = launch_cli(
+            bullet(
+                name="pkg",
+                prompt="Choose the package manager:",
+                choices=list(Packaging),
+            ),
+            bullet(name="py", prompt="Select Python version:", choices=PYTHON_VERSIONS),
         )
-        result = cli.launch()
-        context = Context(name=name, packaging=result[0][1], python=result[1][1])
+        context = Context(name=name, packaging=result["pkg"], python=result["py"])
     generate_project(context)
 
 
