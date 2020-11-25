097eb1124ac175774b656c6777a3c3a5c75e6edb
Marcelo Trylesinski <marcelotryle@gmail.com>
2020-11-25 03:13:09 +0100

    ✅ Add tests to new interact setup

--- ROADMAP.md
+++ ROADMAP.md
@@ -4,27 +4,29 @@ Hi there! :wave:
 
 The package plans are here. If you want to contribute with new ideas, or develop the ones that are listed, read our contributing guidelines! 🤓
 
-
 ## Checklist
 
 ### Must
-- [X] License support on `startproject`.
-- [X] Docker/Docker-compose support on `startproject`.
-- [X] Add basic linter tools on `startproject` (flake8, mypy and isort).
-- [X] Add `.pre-commit-config.yaml` on `startproject`.
-- [ ] Integrate databases on `startproject`.
-- [ ] Different Authentication support on `startproject`.
-- [ ] Add tests.
+
+* [X] License support on `startproject`.
+* [X] Docker/Docker-compose support on `startproject`.
+* [X] Add basic linter tools on `startproject` (flake8, mypy and isort).
+* [X] Add `.pre-commit-config.yaml` on `startproject`.
+* [X] Integrate databases on `startproject`.
+* [ ] Different Authentication support on `startproject`.
+* [ ] Add tests.
+* [ ] Fix documentation accordingly.
 
 ### Nice to have
-- [ ] VSCode debugger support on `startproject` (available via docker).
-- [ ] Support different CI on `startproject`.
-- [ ] Add support for `hypercorn` on `run`.
-- [ ] Create `migrations`/`migrate` command.
-- [ ] Configuration file support: being able to run `fastapi startproject --config-file myconfig`.
-- [ ] Add `logger` to `startproject` structure.
-- [ ] Base CRUD class to `startproject`.
+
+* [ ] VSCode debugger support on `startproject` (available via docker).
+* [ ] Support different CI on `startproject`.
+* [ ] Add support for `hypercorn` on `run`.
+* [ ] Create `migrations`/`migrate` command.
+* [ ] Add `logger` to `startproject` structure.
+* [ ] Base CRUD class to `startproject`.
 
 ## Questions
 
-- Should we support .git by default?
+* Should we support .git by default?
+* Should CORSMiddleware be optional?
--- tests/test_startproject.py
+++ tests/test_startproject.py
@@ -1,9 +1,8 @@
 from unittest.mock import patch
 
 import pytest
-from typer.testing import CliRunner
-
 from manage_fastapi.main import app
+from typer.testing import CliRunner
 
 runner = CliRunner()
 
@@ -13,12 +12,10 @@ ALREADY_EXISTS = "Folder 'potato' already exists. 😞\n"
 
 @pytest.mark.parametrize("packaging", ["pip", "poetry"])
 @pytest.mark.parametrize("python", ["3.6", "3.7", "3.8"])
-@pytest.mark.parametrize(
-    "license_", ["MIT", "BSD-3", "GNU GPL v3.0", "Apache Software License 2.0"]
-)
+@pytest.mark.parametrize("license_", ["MIT", "BSD", "GNU", "Apache"])
 @pytest.mark.parametrize("pre_commit", [True, False])
 @pytest.mark.parametrize("docker", [True, False])
-@pytest.mark.parametrize("database", ["Postgres", "None"])
+@pytest.mark.parametrize("database", ["Postgres"])
 def test_startproject(
     project_name: str,
     packaging: str,
@@ -42,23 +39,23 @@ def test_startproject(
             }
 
         mock_obj.side_effect = side_effect
-        result = runner.invoke(app, ["startproject", project_name])
+        result = runner.invoke(app, ["startproject", project_name, "--interact"])
         assert mock_obj.assert_called_once
         assert result.output == CREATED_SUCCESSFULLY
         assert result.exit_code == 0
 
 
 def test_startproject_default(project_name: str):
-    result = runner.invoke(app, ["startproject", project_name, "--default"])
+    result = runner.invoke(app, ["startproject", project_name])
     assert result.output == CREATED_SUCCESSFULLY
     assert result.exit_code == 0
 
 
 def test_startproject_already_exists(project_name: str):
-    result = runner.invoke(app, ["startproject", project_name, "--default"])
+    result = runner.invoke(app, ["startproject", project_name])
     assert result.output == CREATED_SUCCESSFULLY
     assert result.exit_code == 0
 
-    result = runner.invoke(app, ["startproject", project_name, "--default"])
+    result = runner.invoke(app, ["startproject", project_name])
     assert result.output == ALREADY_EXISTS
     assert result.exit_code == 0
