bfe1b6bbf5b077a7259b18bc19fd74901b63a39b
Marcelo Trylesinski <marcelotryle@gmail.com>
2021-09-02 00:12:13 +0200

    Add questionary dependency

--- manage_fastapi/helpers.py
+++ manage_fastapi/helpers.py
@@ -1,37 +1,27 @@
 import re
-from typing import Tuple, TypeVar
+from typing import Any, Dict, TypeVar, cast
 
-from bullet import Bullet, SlidePrompt, colors
-from bullet.client import YesNo
+import questionary
 
 EnumType = TypeVar("EnumType")
-
-WHITE_FOREGROUND = colors.foreground["white"]
 
 
 def camel_to_snake(text: str) -> str:
     return re.sub(r"(?<!^)(?=[A-Z])", "_", text).lower()
 
 
-def bullet(choices: EnumType) -> Bullet:
+def question(choices: EnumType) -> questionary.Question:
     prompt = camel_to_snake(choices.__name__).replace("_", " ")  # type: ignore
-    return Bullet(
-        prompt=f"Select the {prompt}: ",
-        choices=list(choices),  # type: ignore
-        bullet=" >",
-        margin=2,
-        word_color=WHITE_FOREGROUND,
-        word_on_switch=WHITE_FOREGROUND,
-    )
+    return questionary.select(f"Select the {prompt}: ", choices=list(choices))
 
 
-def yes_no(option: str) -> YesNo:
-    return YesNo(
-        prompt=f"Do you want {option}?", default="n", word_color=WHITE_FOREGROUND
-    )
+def binary_question(option: str) -> questionary.Question:
+    return questionary.confirm(f"Do you want {option}?", default=False)
 
 
-def launch_cli(*prompt_objs: Tuple[str, Bullet]):
-    names, objs = zip(*prompt_objs)
-    results = SlidePrompt(objs).launch()
-    return {name: result[1] for name, result in zip(names, results)}
+def launch_cli(**kwargs: Any) -> Dict[str, Any]:
+    result = {}
+    for name, question in kwargs.items():
+        question = cast(questionary.Question, question)
+        result[name] = question.ask()
+    return result
